57d6212404a3038b7a19571aff20e4b5faf2de7c
Wang1 <Wenbo.Wang1@kuka.com>
2019-09-17 14:11:40 +0800

    add hero selector

---
+++ src/app/hero/selectors/hero.selector.ts
@@ -0,0 +1,33 @@
+import {
+    createSelector,
+    createFeatureSelector,
+    ActionReducerMap,
+} from '@ngrx/store';
+
+import * as fromHero from '../reducers/hero.reducer';
+
+export interface State {
+    heroes: fromHero.State,
+}
+
+export const reducers: ActionReducerMap<State> = {
+    heroes: fromHero.reducer,
+};
+
+export const selectHeroState = createFeatureSelector<fromHero.State>(fromHero.heroesFeatureKey);
+
+export const selectHeroIds = createSelector(selectHeroState, fromHero.selectIds);
+
+export const selectHeroEntities = createSelector(selectHeroState, fromHero.selectEntities);
+
+export const selectAllHeroes = createSelector(selectHeroState, fromHero.selectAll);
+
+export const selectHeroTotal = createSelector(selectHeroState, fromHero.selectTotal);
+
+export const selectCurrentHeroId = createSelector(selectHeroState, fromHero.getSelectedHeroId);
+
+export const selectCurrentHero = createSelector(
+    selectHeroEntities,
+    selectCurrentHeroId,
+    (heroEntities, heroId) => heroEntities[heroId]
+);
