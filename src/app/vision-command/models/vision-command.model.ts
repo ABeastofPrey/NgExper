ed89fc8e7acb2012f51a2cc0f669bdf063cfe7c5
Wang1 <Wenbo.Wang1@kuka.com>
2019-11-13 14:42:13 +0800

    comment vision-command

--- src/app/vision-command/models/vision-command.model.ts
+++ src/app/vision-command/models/vision-command.model.ts
@@ -1,30 +1,30 @@
-import { CommandOptions } from '../enums/command-type.enum';
+// import { CommandOptions } from '../enums/command-type.enum';
 
-export interface VisionCommand {
-    id: number,
+// export interface VisionCommand {
+//     id: number,
 
-    [CommandOptions.Station]: string;
+//     [CommandOptions.Station]: string;
 
-    jobNames: string[];
-    [CommandOptions.Job]: string;
+//     jobNames: string[];
+//     [CommandOptions.Job]: string;
 
-    dimensions: string[];
-    [CommandOptions.Dimension]: string;
+//     dimensions: string[];
+//     [CommandOptions.Dimension]: string;
 
-    dataNums: string[];
-    [CommandOptions.DataNum]: string;
+//     dataNums: string[];
+//     [CommandOptions.DataNum]: string;
 
-    asDatas: string[];
-    [CommandOptions.AsData]: string;
+//     asDatas: string[];
+//     [CommandOptions.AsData]: string;
 
-    statusList: string[];
-    [CommandOptions.Status]: string;
+//     statusList: string[];
+//     [CommandOptions.Status]: string;
 
-    errors: string[];
-    [CommandOptions.Error]: string;
+//     errors: string[];
+//     [CommandOptions.Error]: string;
 
-    variables: string[];
-    [CommandOptions.Variable]: string;
+//     variables: string[];
+//     [CommandOptions.Variable]: string;
 
-    [CommandOptions.Timeout]: number;
-}
+//     [CommandOptions.Timeout]: number;
+// }
--- src/app/vision-command/selectors/vision-command.selectors.ts
+++ src/app/vision-command/selectors/vision-command.selectors.ts
@@ -1,17 +1,17 @@
-import { createSelector, createFeatureSelector } from '@ngrx/store';
-import { State as VisionCommandState } from '../entities/vision-command.entity';
-import { visionCommandFeatureKey, selectAll, selectEntities, getSelectedVisionCommandId } from '../reducers/vision-command.reducer';
+// import { createSelector, createFeatureSelector } from '@ngrx/store';
+// import { State as VisionCommandState } from '../entities/vision-command.entity';
+// import { visionCommandFeatureKey, selectAll, selectEntities, getSelectedVisionCommandId } from '../reducers/vision-command.reducer';
 
-export const selectVisionCommand = createFeatureSelector<VisionCommandState>(visionCommandFeatureKey);
+// export const selectVisionCommand = createFeatureSelector<VisionCommandState>(visionCommandFeatureKey);
 
-export const selectAllVisionCommand = createSelector(selectVisionCommand, selectAll);
+// export const selectAllVisionCommand = createSelector(selectVisionCommand, selectAll);
 
-export const selectVisionCommandEntities = createSelector(selectVisionCommand, selectEntities);
+// export const selectVisionCommandEntities = createSelector(selectVisionCommand, selectEntities);
 
-export const selectCurrentVisionCommandId = createSelector(selectVisionCommand, getSelectedVisionCommandId);
+// export const selectCurrentVisionCommandId = createSelector(selectVisionCommand, getSelectedVisionCommandId);
 
-export const selectCurrentVisionCommand = createSelector(
-    selectVisionCommandEntities,
-    selectCurrentVisionCommandId,
-    (entities, id) => entities[id]
-);
+// export const selectCurrentVisionCommand = createSelector(
+//     selectVisionCommandEntities,
+//     selectCurrentVisionCommandId,
+//     (entities, id) => entities[id]
+// );
